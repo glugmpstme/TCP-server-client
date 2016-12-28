a5aacb6ea28e288983c9060af5fb47752f802980
Pranjali Basmatkar <basmatkarpranjali@gmail.com>
2016-12-28 23:05:54 +0530

    Update server.c

--- server.c
+++ server.c
@@ -1,23 +1,27 @@
-/*socket()---bind()---listen()---accept() */
+/*socket()---bind()---listen()---accept()---read()---write() */
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 #include<sys/socket.h>
+#include<sys/types.h>
 #include<netinet/in.h>
+
 
 void error(char *msg)
 {
-cout<<msg;
-    exit();
+    perror(msg);
+    exit(1);
 }
 int main(int argc,char *argv[])
 {
 char hostname[100];
 char ipadd[100];
 char buff[500];
-int sockfd,portno;
-struct sockaddr_in serv_addr;
+int sockfd,newsockfd,portno;
+struct sockaddr_in serv_addr , cli_addr;
 
 /*
-   sin_family, sin_port,sin_zero[8] 
+   sin_family, sin_port,sin_zero[8]
 */
 //using the socket() function
 sockfd=socket(AF_INET,SOCK_STREAM,0);
@@ -36,7 +40,16 @@ if(bind(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr))==-1)
 listen(sockfd,10);
 //no need to check for error as socket argument has to be valid by this point.
 
+newsockfd=accept(sockfd, (struct sockaddr *)&cli_addr, &sizeof(cli_addr));
+if(newsockfd==-1)
+    error('error');
+if(n=read(newsockfd, buff,200) < 0)
+    error('error reading');
+printf("%s",buff);
 
+if(n=write(newsockfd,"vgmvhghmg",10) < 0)
+    error('error reading');
 
+return 0;
 }
 

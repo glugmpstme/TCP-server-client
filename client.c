445402d6b7ea2648434c92c7079446adcefe1173
Pranjali Basmatkar <basmatkarpranjali@gmail.com>
2017-01-04 22:42:45 +0530

    Create client.c

---
+++ client.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<sys/socket.h>
+#include<sys/types.h>
+#include<netinet/in.h>
+
+void error(char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+
+int main(int argc, char *argv[])
+{
+    int sockfd;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+    char buffer[500];
+    sockfd=socket(AF_INET,SOCK_STREAM,0);
+    serv_addr.sin_family=AF_INET;
+    serv_addr.sin_addr.s_addr=inet_addr("127.0.0.1");
+    serv_addr.sin_port=htons(SERV_TCP_PORT);
+    
+}

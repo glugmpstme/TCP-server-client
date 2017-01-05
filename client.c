9907db2baa600890c4ebd241fc380c3e856c42ec
Pranjali Basmatkar <basmatkarpranjali@gmail.com>
2017-01-05 18:15:09 +0530

    Update client.c

--- client.c
+++ client.c
@@ -21,5 +21,7 @@ int main(int argc, char *argv[])
     serv_addr.sin_family=AF_INET;
     serv_addr.sin_addr.s_addr=inet_addr("127.0.0.1");
     serv_addr.sin_port=htons(SERV_TCP_PORT);
+    connect(sockfd,(struct sockaddr*)&serv_addr,sizeof(serv_addr));
     
+
 }

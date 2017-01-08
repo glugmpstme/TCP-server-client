bd7b4beb37c79291b70f8580000a4c16c4c49b6a
Pranjali Basmatkar <basmatkarpranjali@gmail.com>
2017-01-08 17:36:23 +0530

    Update server.c

--- server.c
+++ server.c
@@ -14,7 +14,8 @@ void error(char *msg)
 }
 int main(int argc,char *argv[])
 {
-
+char hostname[100];
+char ipadd[100];
 char buff[500];
 int sockfd,newsockfd,portno;
 struct sockaddr_in serv_addr , cli_addr;
@@ -46,7 +47,10 @@ if(n=read(newsockfd, buff,200) < 0)
     error('error reading');
 printf("%s",buff);
 
-
+/*if(n=write(newsockfd,"vgmvhghmg",10) < 0)
+    error('error reading');
+*/
+close(sockfd);
 return 0;
 }
 

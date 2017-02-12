7c5e9debb314e9e07b22914fd1d88b4b994956cb
Pranjali Basmatkar <basmatkarpranjali@gmail.com>
2017-02-12 21:14:26 +0530

    no more compilation errors but error "connecting" during execution :'(

--- client.c
+++ client.c
@@ -1,9 +1,14 @@
+
+
 #include<stdio.h>
 #include<stdlib.h>
+#include<unistd.h>
 #include<string.h>
 #include<sys/socket.h>
 #include<sys/types.h>
 #include<netinet/in.h>
+#include<arpa/inet.h>
+
 
 void error(char *msg)
 {
@@ -13,17 +18,35 @@ void error(char *msg)
 
 int main(int argc, char *argv[])
 {
-    int sockfd;
+    int sockfd,var,portno;
     struct sockaddr_in serv_addr;
     struct hostent *server;
-    char buffer[500];
+
+    char buff[500];
+    portno=atoi(argv[2]);  
+    server= gethostbyname(argv[1]);
+
     sockfd=socket(AF_INET,SOCK_STREAM,0);
+    if(sockfd < 0)
+    	error("socket won't open");
+
     serv_addr.sin_family=AF_INET;
-    serv_addr.sin_addr.s_addr=inet_addr("127.0.0.1");
-    serv_addr.sin_port=htons(SERV_TCP_PORT);
-    connect(sockfd,(struct sockaddr*)&serv_addr,sizeof(serv_addr));
-    //input message in buff of size
-    write(sockfd,buff,size);
+    
+    bcopy((char *)server->h_addr,((char *)&serv_addr.sin_addr.s_addr,server->h_length));
+    
+    serv_addr.sin_port = htons(portno);
+    if (connect(sockfd,(struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+        error("error connecting");
+    printf("Enter the message: ");
+    fgets(buff,500,stdin);
+    var=write(sockfd,buff,strlen(buff));
+    if(var<0)
+    	error("error writing");
+    bzero(buff,500);
+    var= read(sockfd,buff,500);
+    if(var<0)
+    	error("error reading");
+    printf("%s",buff);
 
     close(sockfd);
 return 0;

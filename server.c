80114ba937d8de2c83897bda3b6ff88c61cff388
Pranjali Basmatkar <basmatkarpranjali@gmail.com>
2017-02-12 20:12:08 +0530

    fixed compilation errors

--- server.c
+++ server.c
@@ -2,6 +2,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include<unistd.h>
 #include<sys/socket.h>
 #include<sys/types.h>
 #include<netinet/in.h>
@@ -17,7 +18,7 @@ int main(int argc,char *argv[])
 char hostname[100];
 char ipadd[100];
 char buff[500];
-int sockfd,newsockfd,portno;
+int sockfd,newsockfd,portno,clength,var;
 struct sockaddr_in serv_addr , cli_addr;
 
 /*
@@ -34,22 +35,22 @@ serv_addr.sin_family = AF_INET;
 serv_addr.sin_port = htons(portno);
 serv_addr.sin_addr.s_addr = INADDR_ANY;
 //using the bind() function
-if(bind(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr))==-1)
+if(bind(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr))<0)
     error("error in binding");
 //using the listen() function
 listen(sockfd,10);
 //no need to check for error as socket argument has to be valid by this point.
-
-newsockfd=accept(sockfd, (struct sockaddr *)&cli_addr, &sizeof(cli_addr));
+clength= sizeof(cli_addr);
+newsockfd=accept(sockfd, (struct sockaddr *)&cli_addr,&clength);
 if(newsockfd==-1)
-    error('error');
-if(n=read(newsockfd, buff,200) < 0)
-    error('error reading');
+    error("error");
+if(var=read(newsockfd, buff,200) < 0)
+    error("error reading");
 printf("%s",buff);
 
-/*if(n=write(newsockfd,"vgmvhghmg",10) < 0)
-    error('error reading');
-*/
+if(var=write(newsockfd,"maacarena",10) < 0)
+    error("error writing");
+
 close(sockfd);
 return 0;
 }

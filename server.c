455ed0b25c3a06706e5db17d01c128a3e33c27b5
Pranjali Basmatkar <basmatkarpranjali@gmail.com>
2017-02-28 16:52:21 +0530

    Update server.c

--- server.c
+++ server.c
@@ -13,43 +13,54 @@ void error(char *msg)
     perror(msg);
     exit(1);
 }
+
 int main(int argc,char *argv[])
 {
-char hostname[100];
-char ipadd[100];
 char buff[500];
-int sockfd,newsockfd,portno,clength,var;
+int sockfd,newsockfd,portno,clength,var,flag=0;
 struct sockaddr_in serv_addr , cli_addr;
 
 /*
    sin_family, sin_port,sin_zero[8]
 */
 //using the socket() function
-sockfd=socket(AF_INET,SOCK_STREAM,0);
-if(sockfd==-1)
-{
+sockfd = socket(AF_INET,SOCK_STREAM,0);
+if(sockfd < 0)
     error("socket won't work");
-}
+//assigning all the values
 portno = atoi(argv[1]);
 serv_addr.sin_family = AF_INET;
 serv_addr.sin_port = htons(portno);
 serv_addr.sin_addr.s_addr = INADDR_ANY;
+
 //using the bind() function
 if(bind(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr))<0)
     error("error in binding");
+
 //using the listen() function
 listen(sockfd,10);
-//no need to check for error as socket argument has to be valid by this point.
+
+//using the accept function
 clength= sizeof(cli_addr);
 newsockfd=accept(sockfd, (struct sockaddr *)&cli_addr,&clength);
-if(newsockfd==-1)
-    error("error");
-if(var=read(newsockfd, buff,200) < 0)
-    error("error reading");
-printf("%s",buff);
 
-if(var=write(newsockfd,"maacarena",10) < 0)
+if(newsockfd < 0)
+    error("error");
+bzero(buff,200);
+//read function
+
+var=read(newsockfd,buff,200);
+if(var<0)
+    error("error reading");
+//printing the stuff
+printf("%s",buff);
+//writing back to the server
+printf("enter the reply\n");
+fgets(buff,500,stdin);
+var=write(newsockfd,buff,500);
+if(var < 0)
     error("error writing");
+
 
 close(sockfd);
 return 0;
